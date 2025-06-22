b5ff3811b58cd9c07d1631797b109aa1000ded52
lokesh <annelokesh3011@gmail.com>
2025-06-22 13:03:37 -0400

    4records

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,5 @@
 INSERT INTO login (user_Name, password) VALUES ('admin', 'password123');
 INSERT INTO login (user_Name, password) VALUES ('lokesh', 'lokesh123');
+INSERT INTO login (user_Name, password) VALUES ('lokesh12', 'lokesh123');
+INSERT INTO login (user_Name, password) VALUES ('lokesh34', 'lokesh123');
+INSERT INTO login (user_Name, password) VALUES ('lokesh56', 'lokesh123');
